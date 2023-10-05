1cc670c7dcedfddafe27d0d02a9492362c32b6af
Gabriel Galeski <galeski35@gmail.com>
2023-10-05 14:47:37 -0300

    feat: implementa testes para class ingredient

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,18 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, Restriction
 
 
 # Req 1
 def test_ingredient():
-    pass 
+    ingredient = Ingredient("queijo mussarela")
+    ingredient2 = Ingredient("queijo mussarela")
+    ingredient4 = Ingredient("farinha")
+    assert ingredient.name == "queijo mussarela"
+    assert ingredient.restrictions == {
+        Restriction.LACTOSE,
+        Restriction.ANIMAL_DERIVED,
+    }
+    assert hash(ingredient) == hash(ingredient2)
+    assert hash(ingredient) != hash(ingredient4)
+    assert ingredient == ingredient2
+    assert ingredient != ingredient4
+    assert repr(ingredient) == "Ingredient('queijo mussarela')"
