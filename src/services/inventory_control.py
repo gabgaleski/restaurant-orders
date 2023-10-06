f755c47f6d62e3f218c4f039a3f6c48574b5b10e
Gabriel Galeski <galeski35@gmail.com>
2023-10-06 14:45:51 -0300

    feat: implementa inventory control

--- src/services/inventory_control.py
+++ src/services/inventory_control.py
@@ -27,8 +27,18 @@ class InventoryMapping:
 
     # Req 5.1
     def check_recipe_availability(self, recipe: Recipe) -> bool:
-        pass
+        for ingredient, quantity in recipe.items():
+            if (
+                ingredient not in self.inventory
+                or self.inventory[ingredient] < quantity
+            ):
+                return False
+        return True
 
     # Req 5.2
     def consume_recipe(self, recipe: Recipe) -> None:
-        pass
+        if not self.check_recipe_availability(recipe):
+            raise ValueError("Receita não disponível")
+
+        for ingredient, quantity in recipe.items():
+            self.inventory[ingredient] -= quantity
